2e528c26553f28278d44268a62a570329ce77899
Nikolett Palyi <nikpalyi@gmail.com>
2020-01-30 14:01:39 +0000

    Updates

---
+++ precache-manifest.17430fecf12ce761001992515769eb0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468a8da0f096ec79a90a6f57625b2c9a",
+    "url": "/nikpalyi/video-player-react/index.html"
+  },
+  {
+    "revision": "6ea537fed8438fd271be",
+    "url": "/nikpalyi/video-player-react/static/css/main.b76b8b4d.chunk.css"
+  },
+  {
+    "revision": "90fce1be17ca92394a4f",
+    "url": "/nikpalyi/video-player-react/static/js/2.2bfc79a7.chunk.js"
+  },
+  {
+    "revision": "6ea537fed8438fd271be",
+    "url": "/nikpalyi/video-player-react/static/js/main.6d2bb907.chunk.js"
+  },
+  {
+    "revision": "e1104ba057ead309bac7",
+    "url": "/nikpalyi/video-player-react/static/js/runtime-main.9e8a6934.js"
+  }
+]);
